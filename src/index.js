5b96565d21224b2850b63e6816f6c46c36fe4e02
Frank-Kawp <alexandr.voronetskiy@gmail.com>
2018-09-11 11:31:45 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
 import readLineSync from 'readline-sync';
 
-console.log(`Welcome to the Brain Games!`);
+console.log('Welcome to the Brain Games!');
+let userName = '';
 
 
 const sayHiToNewUser = () => {
-  const userName = readLineSync.question('May I have your name? ');
+  const userAnswer = readLineSync.question('May I have your name? ');
+  userName = userAnswer;
   console.log(`Hello, ${userName}`);
-  return userName;
+  console.log(' ');
 };
 
 export const isEvenGame = () => {
-  console.log(`Answer "yes" if number even otherwise answer "no".`);
-  console.log(` `);
+  console.log('Answer "yes" if number even otherwise answer "no".');
+  console.log(' ');
 
-  const userName = sayHiToNewUser();
-  console.log(` `);
-
+  sayHiToNewUser();
   let counter = 0;
 
   while (counter !== 3) {
-  	const quessNumber = Math.floor(100 * Math.random() * 4);  	
-    const correctAnswer = (quessNumber % 2 === 0) ? `yes` : `no`;
+    const quessNumber = Math.floor(100 * Math.random() * 4);
+    const correctAnswer = (quessNumber % 2 === 0) ? 'yes' : 'no';
 
-  	console.log(`Question: ${quessNumber}`);
-  	const userAnswer = readLineSync.question(`Your answer: `);
+    console.log(`Question: ${quessNumber}`);
+    const userAnswer = readLineSync.question('Your answer: ');
 
-  	if (correctAnswer === userAnswer) {
-  		console.log(`Correct!`);
-  		counter += 1;
-  	} else {
-  		console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}`);
-  		console.log(`Let's try again, ${userName}`);
-  	}
+    if (correctAnswer === userAnswer) {
+      console.log('Correct!');
+      counter += 1;
+    } else {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}`);
+      console.log(`Let's try again, ${userName}`);
+    }
   }
   console.log(`Congratulations, ${userName}`);
 };
