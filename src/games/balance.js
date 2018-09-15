f2118cd590296c65ce50cb91f8166303c3a6e4a8
Frank-Kawp <alexandr.voronetskiy@gmail.com>
2018-09-15 21:38:26 +0300

    update balance game

--- src/games/balance.js
+++ src/games/balance.js
@@ -7,19 +7,20 @@ const description = 'Balance the given number.';
 const balance = (str) => {
   const summ = str.split('').reduce((sum, current) => Number(sum) + Number(current));
   const average = Math.floor(summ / str.length);
-  const summ2 = average * str.length;
-  let diff = summ - summ2;
-  const result = [];
+  const diff = summ - (average * str.length);
+  let result = '';
 
-  while (result.length !== str.length) {
-    result.push(average);
+  switch (diff) {
+    case 1:
+      result = String(average).repeat(str.length - 1) + (average + 1);
+      return String(result);
+    case 2:
+      result = String(average).repeat(str.length - 2) + (average + 1) + (average + 1);
+      return String(result);
+    default:
+      result = String(average).repeat(str.length);
+      return String(result);
   }
-
-  for (let i = 0; diff !== 0; i += 1) {
-    result[i] += 1;
-    diff -= 1;
-  }
-  return result.sort((a, b) => a - b).join('');
 };
 
 
